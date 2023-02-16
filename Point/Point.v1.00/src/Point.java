2a41ad9ecb976a97e544803ee230ea38002d2914
Larry Clement <lclement@calbaptist.edu>
2023-02-16 10:32:41 -0800

    Final commit after class (including methods from hidden slides)

--- Point/Point.v1.00/src/Point.java
+++ Point/Point.v1.00/src/Point.java
@@ -1,3 +1,33 @@
-public class Point {
+import java.awt.*;
 
+public class Point {
+    int x;
+    int y;
+
+    public Point() {
+        this(0,0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(Graphics g) {
+        g.fillOval(x, y, 3, 3);
+        g.drawString("( " + x + ", " + y + " )", x + 5, y - 5);
+    }
+
+    public void translate(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public double distanceFromOrigin() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
--- Point/Point.v1.00/src/PointMain.java
+++ Point/Point.v1.00/src/PointMain.java
@@ -1,7 +1,24 @@
+import java.awt.*;
+
 public class PointMain {
 
     public static void main(String[] args) {
+        DrawingPanel panel = new DrawingPanel(300, 300);
+        Graphics g = panel.getGraphics();
 
+        Point p1 = new Point();
+        Point p2 = new Point(20, 100);
+
+        System.out.println("Point 1 is " + p1);
+        System.out.println("Point 2 is " + p2);
+
+        p2.translate(-3, 6);    // moves p2 left 3 and down 6
+        System.out.println("Point 2 is " + p2);
+
+        System.out.println("Point 2 is " + p2.distanceFromOrigin() + " pixels from the origin (0,0)");
+
+        p1.draw(g);
+        p2.draw(g);
     }
 
 }
